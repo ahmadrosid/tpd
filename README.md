0feb19cd9916dbb77f705ae36533aff419a24d70
ahmadrosid <alahmadrosid@gmail.com>
2022-01-30 00:46:32 +0700

    feat: add some option and help

---
+++ README.md
@@ -0,0 +1,23 @@
+# tpd
+Typo detector for english words. The default dictionary words from [here](https://github.com/first20hours/google-10000-english/blob/master/20k.txt)
+
+If you want to add more words you can add config file at `$HOME/.config/tpd/dictionary`.
+
+## Usage
+```bash
+Typo detector for english words
+
+USAGE:
+    tpd [FILE_PATH] [SUBCOMMAND]
+
+ARGS:
+    <FILE_PATH>    File path to read
+
+OPTIONS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+
+SUBCOMMANDS:
+    help     Print this message or the help of the given subcommand(s)
+    words    Store custom word to dictionary
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{IntoApp, Parser, Subcommand};
+use std::io::{Read, Write};
+
+/// Typo detector for english words
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// File path to read
+    file_path: Option<String>,
+
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Store custom word to dictionary
+    Words {
+        /// English word
+        #[clap(short, long)]
+        word: Option<String>,
+
+        /// Display my dictionary
+        #[clap(short, long)]
+        d: bool,
+    },
+}
 
 trait CharExt {
     fn accept(self) -> bool;
@@ -7,10 +34,110 @@ impl CharExt for char {
     fn accept(self) -> bool {
         match self {
             'a'..='z' | 'A'..='Z' | '\'' => true,
-            _ => false
+            _ => false,
         }
     }
 }
+
+fn main() {
+    let args = Args::parse();
+    match &args.command {
+        Some(Commands::Words { word, d }) => {
+            match home::home_dir() {
+                Some(mut path) => {
+                    path.push(".config");
+                    path.push("tpd");
+                    path.push("dictionary");
+
+                    let mut file = std::fs::OpenOptions::new()
+                        .write(true)
+                        .read(true)
+                        .append(true)
+                        .open(path)
+                        .unwrap();
+
+                    if *d {
+                        let mut data = String::new();
+                        if let Ok(_) = file.read_to_string(&mut data) {
+                            println!("{}", data);
+                        } else {
+                            println!("Unable to read dictionary");
+                        }
+                    } else {
+                        write!(file, "{}\n", word.as_deref().unwrap());
+                        println!("Success store '{}' to dictionary!", word.as_deref().unwrap());
+
+                    }
+                }
+                None => {}
+            }
+
+            std::process::exit(0);
+        }
+        None => {}
+    }
+
+    match args.file_path.as_deref() {
+        Some(path) => detect_typo(path.to_string()),
+        _ => {
+            let mut app = Args::into_app();
+            app.print_help();
+        }
+    }
+}
+
+pub fn detect_typo(file_path: String) {
+    match std::fs::read_to_string(file_path) {
+        Ok(source) => {
+            let val = include_str!("words_sort.txt").to_string();
+            let mut words: Vec<&str> = val.split("\n").collect();
+
+            match home::home_dir() {
+                Some(mut path) => {
+                    path.push(".config");
+                    path.push("tpd");
+                    path.push("dictionary");
+                    match std::fs::read_to_string(path) {
+                        Ok(file) => {
+                            let dictionary = file.clone();
+                            let dictionaries = dictionary.split("\n").collect::<Vec<_>>();
+                            words.append(&mut dictionaries.to_vec());
+                            words.sort();
+                            scan_words(&*source, &words);
+                            std::process::exit(0);
+                        }
+                        _ => {}
+                    }
+                }
+                None => {}
+            }
+
+            scan_words(&*source, &words);
+        }
+        Err(_) => {
+            println!("File not found!");
+            std::process::exit(1);
+        }
+    }
+}
+
+pub fn scan_words(source: &str, words: &Vec<&str>) {
+    let mut index = 0;
+    let file = std::env::args().collect::<Vec<_>>();
+    for line in source.lines().into_iter() {
+        index += 1;
+        for child in line.split(" ").into_iter() {
+            let target = child.to_lowercase();
+            if !target.chars().all(char::accept) {
+                continue;
+            }
+            if search(&words.to_vec(), &target, words.len()).is_none() {
+                println!("\"{}\" => {}:{}", child, file[1], index);
+            }
+        }
+    }
+}
+
 pub fn search(a: &Vec<&str>, target_value: &str, len: usize) -> Option<usize> {
     let mut low: usize = 0;
     let mut high: usize = len - 1;
@@ -33,37 +160,4 @@ pub fn search(a: &Vec<&str>, target_value: &str, len: usize) -> Option<usize> {
         }
     }
     None
-}
-
-fn main() {
-    let mut arg = std::env::args();
-    if arg.len() == 1 {
-        println!("Please provide file path!");
-        std::process::exit(0x1)
-    }
-    arg.next();
-    match std::fs::read_to_string(arg.next().unwrap()) {
-        Ok(source) => {
-            let val = include_str!("words_sort.txt").to_string();
-            let words: Vec<&str> = val.split("\n").collect();
-            let mut index = 0;
-            let file = std::env::args().collect::<Vec<_>>();
-            for line in source.lines().into_iter() {
-                index += 1;
-                for child in line.split(" ").into_iter() {
-                    let target = child.to_lowercase();
-                    if !target.chars().all(char::accept) {
-                        continue;
-                    }
-                    if search(&words, &target, words.len()).is_none() {
-                        println!("\"{}\" => {}:{}", child, file[1], index);
-                    }
-                }
-            }
-        }
-        Err(_) => {
-            println!("File not found!");
-            std::process::exit(1);
-        }
-    }
 }

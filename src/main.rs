c07e1899e6390f374beca5278352fe0ac5f15f7f
ahmadrosid <alahmadrosid@gmail.com>
2022-01-31 21:56:39 +0700

    feat: add suggest fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{IntoApp, Parser, Subcommand};
+use std::cmp::min;
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::io::{Read, Write};
 
 /// Typo detector for english words
@@ -7,6 +10,10 @@ use std::io::{Read, Write};
 struct Args {
     /// File path to read
     file_path: Option<String>,
+
+    /// Suggest fix for typo
+    #[clap(long, short)]
+    fix: bool,
 
     #[clap(subcommand)]
     command: Option<Commands>,
@@ -21,8 +28,8 @@ enum Commands {
         word: String,
 
         /// Display my dictionary
-        #[clap(short)]
-        d: bool,
+        #[clap(long, short)]
+        dictionary: bool,
     },
 }
 
@@ -42,7 +49,7 @@ impl CharExt for char {
 fn main() {
     let args = Args::parse();
     match &args.command {
-        Some(Commands::Add { word, d }) => {
+        Some(Commands::Add { word, dictionary }) => {
             match home::home_dir() {
                 Some(mut path) => {
                     path.push(".config");
@@ -56,7 +63,7 @@ fn main() {
                         .open(path)
                         .unwrap();
 
-                    if *d {
+                    if *dictionary {
                         let mut data = String::new();
                         if let Ok(_) = file.read_to_string(&mut data) {
                             println!("{}", data);
@@ -82,7 +89,7 @@ fn main() {
     }
 
     match args.file_path.as_deref() {
-        Some(path) => detect_typo(path.to_string()),
+        Some(path) => detect_typo(path.to_string(), args.fix),
         _ => {
             let mut app = Args::into_app();
             app.print_help();
@@ -90,7 +97,7 @@ fn main() {
     }
 }
 
-pub fn detect_typo(file_path: String) {
+pub fn detect_typo(file_path: String, show_fix: bool) {
     match std::fs::read_to_string(file_path) {
         Ok(source) => {
             let val = include_str!("words_sort.txt").to_string();
@@ -107,7 +114,7 @@ pub fn detect_typo(file_path: String) {
                             let dictionaries = dictionary.split("\n").collect::<Vec<_>>();
                             words.append(&mut dictionaries.to_vec());
                             words.sort();
-                            scan_words(&*source, &words);
+                            scan_words(&*source, &words, show_fix);
                             std::process::exit(0);
                         }
                         _ => {}
@@ -116,7 +123,7 @@ pub fn detect_typo(file_path: String) {
                 None => {}
             }
 
-            scan_words(&*source, &words);
+            scan_words(&*source, &words, show_fix);
         }
         Err(_) => {
             println!("File not found!");
@@ -125,7 +132,7 @@ pub fn detect_typo(file_path: String) {
     }
 }
 
-pub fn scan_words(source: &str, words: &Vec<&str>) {
+pub fn scan_words(source: &str, words: &Vec<&str>, show_fix: bool) {
     let mut line_number = 0;
     let file = std::env::args().collect::<Vec<_>>();
     for line in source.lines().into_iter() {
@@ -133,12 +140,33 @@ pub fn scan_words(source: &str, words: &Vec<&str>) {
         let mut column = 0;
         for child in line.split(" ").into_iter() {
             let target = child.to_lowercase();
-            column += child.len() +  1;
+            column += child.len() + 1;
             if !target.chars().all(char::accept) {
                 continue;
             }
             if search(&words.to_vec(), &target, words.len()).is_none() {
-                println!("\"{}\" => {}:{}:{}", child, file[1], line_number, column);
+                if !show_fix {
+                    println!("\"{}\" => {}:{}:{}", child, file[1], line_number, column);
+                    continue;
+                }
+
+                match search_similar(&words.to_vec(), &target, 1) {
+                    Some(result) => {
+                        println!(
+                            "\"{}\" => {}:{}:{} {}",
+                            child, file[1], line_number, column, result
+                        )
+                    }
+                    _ => match search_similar(&words.to_vec(), &target, 2) {
+                        Some(result) => {
+                            println!(
+                                "\"{}\" => {}:{}:{} {}",
+                                child, file[1], line_number, column, result
+                            )
+                        }
+                        _ => println!("\"{}\" => {}:{}:{}", child, file[1], line_number, column),
+                    },
+                }
             }
         }
     }
@@ -166,4 +194,97 @@ pub fn search(a: &Vec<&str>, target_value: &str, len: usize) -> Option<usize> {
         }
     }
     None
+}
+
+pub fn search_similar(words: &Vec<&str>, target: &str, score: usize) -> Option<String> {
+    let mut result: String = String::new();
+    let distance = if score == 2 { 0 } else { 1 };
+    words
+        .to_vec()
+        .iter()
+        .filter(|item| item.len() >= target.len() && item.len() <= target.len() + distance)
+        .for_each(|item| {
+            if damerau_levenshtein(*item, &target) == score {
+                result.push_str(*item);
+                result.push(',')
+            }
+        });
+
+    return if result.len() > 0 {
+        Some(result.trim_end_matches(",").to_string())
+    } else {
+        None
+    };
+}
+
+pub fn generic_damerau_levenshtein<Elem>(a_elems: &[Elem], b_elems: &[Elem]) -> usize
+where
+    Elem: Eq + Hash + Clone,
+{
+    let a_len = a_elems.len();
+    let b_len = b_elems.len();
+
+    if a_len == 0 {
+        return b_len;
+    }
+    if b_len == 0 {
+        return a_len;
+    }
+
+    let width = a_len + 2;
+    let mut distances = vec![0; (a_len + 2) * (b_len + 2)];
+    let max_distance = a_len + b_len;
+    distances[0] = max_distance;
+
+    for i in 0..(a_len + 1) {
+        distances[flat_index(i + 1, 0, width)] = max_distance;
+        distances[flat_index(i + 1, 1, width)] = i;
+    }
+
+    for j in 0..(b_len + 1) {
+        distances[flat_index(0, j + 1, width)] = max_distance;
+        distances[flat_index(1, j + 1, width)] = j;
+    }
+
+    let mut elems: HashMap<Elem, usize> = HashMap::with_capacity(64);
+
+    for i in 1..(a_len + 1) {
+        let mut db = 0;
+
+        for j in 1..(b_len + 1) {
+            let k = match elems.get(&b_elems[j - 1]) {
+                Some(&value) => value,
+                None => 0,
+            };
+
+            let insertion_cost = distances[flat_index(i, j + 1, width)] + 1;
+            let deletion_cost = distances[flat_index(i + 1, j, width)] + 1;
+            let transposition_cost =
+                distances[flat_index(k, db, width)] + (i - k - 1) + 1 + (j - db - 1);
+
+            let mut substitution_cost = distances[flat_index(i, j, width)] + 1;
+            if a_elems[i - 1] == b_elems[j - 1] {
+                db = j;
+                substitution_cost -= 1;
+            }
+
+            distances[flat_index(i + 1, j + 1, width)] = min(
+                substitution_cost,
+                min(insertion_cost, min(deletion_cost, transposition_cost)),
+            );
+        }
+
+        elems.insert(a_elems[i - 1].clone(), i);
+    }
+
+    distances[flat_index(a_len + 1, b_len + 1, width)]
+}
+
+fn flat_index(i: usize, j: usize, width: usize) -> usize {
+    j * width + i
+}
+
+pub fn damerau_levenshtein(a: &str, b: &str) -> usize {
+    let (x, y): (Vec<_>, Vec<_>) = (a.chars().collect(), b.chars().collect());
+    generic_damerau_levenshtein(x.as_slice(), y.as_slice())
 }
